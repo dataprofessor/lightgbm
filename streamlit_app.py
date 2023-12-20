217e59a1f1384c83c54cfd34c1472a3a46484407
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 15:44:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ y = df.iloc[:,-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
 
 # Model training
-model = lgb.LGBMClassifier(learning_rate=0.09, max_depth=-5, random_state=42)
+model = lgb.LGBMRegressor(learning_rate=0.09, max_depth=-5, random_state=42)
 
 model.fit(X_train, y_train, eval_set=[(X_test, y_test),(X_train, y_train)],
           verbose=20,eval_metric='logloss')
